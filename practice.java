640f0276057694c10716d34aeabf2f7c73bc33b2
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-02 17:03:17 +0530

    Updated practice.java in GUI

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 public class practice {
     public static void main(String[] args) {
 
-        System.out.println("Changed the first commit");
+        System.out.println("Changed directly here");
     }
 }
