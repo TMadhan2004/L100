0879a68e4f764c180288d5cb43c72b2e16314e09
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-03 10:30:40 +0530

    conflict merge practice

--- practice.java
+++ practice.java
@@ -2,7 +2,6 @@ public class practice {
     public static void main(String[] args) {
 
         System.out.println("one more change");
-        System.out.println("Doing a change for a scenario");
-        System.out.println("Doing a change for a scenario2");
+        System.out.println("conflict merge");
     }
 }
