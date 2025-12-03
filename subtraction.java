f1952811a2edf8f67e4557a0bb8aec0deefd7e92
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-03 10:16:34 +0530

    going to test merge again

--- subtraction.java
+++ subtraction.java
@@ -2,4 +2,8 @@ public class subtraction {
     public int sub(int a, int b) {
         return a - b;
     }
+    public void change(int a)
+    {
+        System.out.println("Using merge this time instead of rebase");
+    }
 }
