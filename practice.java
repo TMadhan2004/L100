66770601bcab682908a373c4f2f07f9ff1be45b9
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-03 10:24:38 +0530

    testing branching

--- practice.java
+++ practice.java
@@ -2,5 +2,6 @@ public class practice {
     public static void main(String[] args) {
 
         System.out.println("one more change");
+        System.out.println("Doing a change for a scenario");
     }
 }
