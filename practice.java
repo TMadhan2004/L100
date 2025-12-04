9f83909d81e04bb81a5946098ecef89f03b8a66f
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-04 16:23:27 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,11 +1,6 @@
 public class practice {
     public static void main(String[] args) {
-
-        System.out.println("one more change");
-        System.out.println("Going to merge");
-        System.out.println("Going to rebase");
-        System.out.println("Using stash");
-
+        System.out.println("Going to face conflict now");
         System.out.println("Going for a new scenario");
     }
 }
