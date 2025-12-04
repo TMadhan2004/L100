f55d7ff5aec27f5a59a51efffedf0f86302e4aba
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-04 15:12:17 +0530

    Going to test recover using reflog

--- practice.java
+++ practice.java
@@ -5,5 +5,6 @@ public class practice {
         System.out.println("conflict merge");
         System.out.println("Made a change to test diff");
         System.out.println("New bug introduced testing revert");
+        System.out.println("Hard reset removed and recovered using reflog");
     }
 }
