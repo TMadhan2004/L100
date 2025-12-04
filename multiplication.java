104509a86a0263db74549dafaea40d1700b533ad
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-04 08:40:07 +0530

    Testing from start for screenshots

--- multiplication.java
+++ multiplication.java
@@ -1,5 +1,9 @@
 public class multiplication {
     public int mul(int a, int b) {
-        return a*b;
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Testing again to store screenshots to docs");
     }
 }
