e7d83b74dce9d8a9027c6865ae1a2a023689e370
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-04 16:24:12 +0530

    Testing conflict cases
    Fixed conflict issues

--- practice.java
+++ practice.java
@@ -1,6 +1,12 @@
 public class practice {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Going to face conflict now");
+=======
+
+        System.out.println("one more change");
+        System.out.println("Going to merge");
+>>>>>>> a22e59a (Testing conflict cases)
         System.out.println("Going for a new scenario");
     }
 }
