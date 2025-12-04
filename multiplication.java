043ac7dccfe4516b720203c69de805336c480276
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-04 08:42:39 +0530

    Updated multiplication.java for pulling

--- multiplication.java
+++ multiplication.java
@@ -2,8 +2,4 @@ public class multiplication {
     public int mul(int a, int b) {
         return a * b;
     }
-
-    public static void main(String[] args) {
-        System.out.println("Testing again to store screenshots to docs");
-    }
 }
