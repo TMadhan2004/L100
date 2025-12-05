f354d5293585132eb6df0ab3c5841da44db64eca
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-05 16:58:20 +0530

    Preparation for release 1.2

--- practice.java
+++ practice.java
@@ -2,8 +2,7 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("Initial version of system");
         System.out.println("Added new features");
-        System.out.println("Going to release");
-        System.out.println("Added a quick small part of a feature");
+        System.out.println("Release 1.2");
 
     }
 }
