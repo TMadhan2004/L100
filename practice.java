6406d68b68f84346554a728e968671747f2947c7
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2025-12-05 16:23:24 +0530

    release notes

--- practice.java
+++ practice.java
@@ -3,6 +3,7 @@ public class practice {
         System.out.println("Initial version of system");
         System.out.println("Added new features");
         System.out.println("bugs too present in feature");
+        System.out.println("Going to release");
 
     }
 }
